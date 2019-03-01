1b92a7187def2a946a632616bb08fc5799722810
Egret99 <Egretliu0909@163.com>
2019-03-01 16:07:41 -0500

    new slider

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,9 +4,10 @@ import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import Card from "./Card"
-import db from "../../DataBase/FirebaseConfig"
-import Loader from "../Loading"
+import Card from "./Card";
+import db from "../../DataBase/FirebaseConfig";
+import Loader from "../Loading";
+import { Fade } from 'react-slideshow-image';
 
 const styles = theme => ({
   root: {
@@ -27,6 +28,76 @@ const styles = theme => ({
   },
 });
 
+//slider
+
+const fadeProperties = {
+  duration: 3000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true
+}
+
+const imgSize = {
+  height: "86.5vh",
+  overflow: "hidden",
+  width: "100%"
+}
+
+const SlideShow = (props) => {
+  console.log(props);
+  const { slider } = props;
+  return (
+    <Fade {...fadeProperties}>
+      <div className="each-fade">
+        <div className="image-container" style={imgSize}>
+          <img 
+            src={slider[0].imgPath} 
+            alt={slider[0].label} 
+            style={{width:"100%"}} 
+          />
+        </div>
+      </div>
+      <div className="each-fade">
+        <div className="image-container" style={imgSize}>
+          <img 
+            src={slider[1].imgPath} 
+            alt={slider[1].label} 
+            style={{width:"100%"}} 
+          />
+        </div>
+      </div>
+      <div className="each-fade">
+        <div className="image-container" style={imgSize}>
+          <img 
+            src={slider[2].imgPath} 
+            alt={slider[2].label} 
+            style={{width:"100%"}} 
+          />
+        </div>
+      </div>
+      <div className="each-fade">
+        <div className="image-container" style={imgSize}>
+          <img 
+            src={slider[3].imgPath} 
+            alt={slider[3].label} 
+            style={{width:"100%"}} 
+          />
+        </div>
+      </div>
+      <div className="each-fade">
+        <div className="image-container" style={imgSize}>
+          <img 
+            src={slider[4].imgPath} 
+            alt={slider[4].label} 
+            style={{width:"100%"}} 
+          />
+        </div>
+      </div>
+    </Fade>
+    );
+}
+
 class IndexSlider extends React.Component {
 
   componentWillMount(){
@@ -42,54 +113,20 @@ class IndexSlider extends React.Component {
     loading:true
   };
 
-  handleNext = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep + 1,
-    }));
-  };
-
-  handleBack = () => {
-    this.setState(prevState => ({
-      activeStep: prevState.activeStep - 1,
-    }));
-  };
 
   render() {
     if(this.state.loading){
       return <Loader />
     }else{
       const { classes, theme } = this.props;
-    const { activeStep,indexData } = this.state;
-    const {slider,card} = indexData
-    const maxSteps = slider.length;
+      const { activeStep,indexData } = this.state;
+      const {slider,card} = indexData;
+      const maxSteps = slider.length;
 
     return (
       <div className={classes.root}>
         <Card data={card}/>
-        <img
-          className={classes.img}
-          src={slider[activeStep].imgPath}
-          alt={slider[activeStep].label}
-        />
-        <MobileStepper
-          steps={maxSteps}
-          variant="text"
-          position="static"
-          activeStep={activeStep}
-          className={classes.mobileStepper}
-          nextButton={
-            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
-              Next
-              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </Button>
-          }
-          backButton={
-            <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
-              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-              Back
-            </Button>
-          }
-        />
+        <SlideShow slider={slider}/>
       </div>
     );
     }
